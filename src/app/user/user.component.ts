950883490c9e61d938a1f831c2a1107dec85c15d
Feihuan Peng <ericrealtor1997@gmail.com>
2025-01-26 23:44:58 -0800

    passing data, dynamic rendering, conditional rendering

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, Input, Output, input, output, computed, EventEmitter } from '@angular/core';
+import { type User } from './user.model';
+// type User = {
+//   id: string;
+//   name: string;
+//   avatar: string;
+// }
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -10,14 +14,26 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex] 
+  // @Input({required: true}) id!: string
+  // @Input({required: true}) avatar!: string
+  // @Input({required: true}) name!: string
+  // avatar = input.required<string>()
+  // name = input.required<string>()
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar
+  @Input({required: true}) user!: User
+  @Input({required:true}) selected!: boolean
+
+  @Output() select = new EventEmitter<string>()
+  // select = output<string>()
+
+
+
+  get imagePath () {
+    return 'assets/users/' + this.user.avatar
   }
+  // imagePath = computed(() => 'assets/users/' + this.avatar())
 
   onSelectUser () {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser = DUMMY_USERS[randomIndex] 
+    this.select.emit(this.user.id)
   }
 }
